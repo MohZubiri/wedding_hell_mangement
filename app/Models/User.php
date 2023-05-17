696887923ee3d4ad11b12ccf23383f03d752fde2
unknown <moh.hak2030@gmail.com>
2023-05-17 07:05:33 +0300

    add auth to project

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\CastsAttribute;
 
 class User extends Authenticatable
 {
@@ -21,6 +26,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type'
     ];
 
     /**
@@ -41,4 +47,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+      /**
+
+     * Interact with the user's first name.
+
+     *
+
+     * @param  string  $value
+
+     * @return IlluminateDatabaseEloquentCastsAttribute
+
+     */
+
+     protected function type(): Attribute
+
+     {
+ 
+         return new Attribute(
+ 
+             get: fn ($value) =>  ["user", "admin", "manager"][$value],
+ 
+         );
+ 
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+
+    Route::get('/home', [HomeUserController::class, 'index'])->name('home');
+
+});
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+
+    Route::get('/admin/home', [HomeAdminController::class, 'adminHome'])->name('admin.home');
+
+});
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:manager'])->group(function () {
+
+    Route::get('/manager/home', [HomeMangerController::class, 'managerHome'])->name('manager.home');
+
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
